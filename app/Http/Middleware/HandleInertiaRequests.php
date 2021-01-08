8541ff5d79e8f6fe6a24c8a091957e9cb894e3d2
Grant Holle <hollegrant@gmail.com>
2021-01-08 22:31:22 +0800

    Add inertia key

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,6 +37,10 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
+            'user' => function () use ($request) {
+                return $request->user()
+                    ->toResource();
+            },
             'flash' => [
                 'success' => session('success'),
                 'error' => session('error'),
