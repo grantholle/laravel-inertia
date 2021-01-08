4763ff69477eca15ee49565a7862f0a890baebae
Grant Holle <hollegrant@gmail.com>
2021-01-08 23:02:00 +0800

    Include the notify plugin

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -1,9 +1,11 @@
 import { plugin } from '@inertiajs/inertia-vue3'
 import http from '@/plugins/http'
 import route from '@/plugins/route'
+import notify from '@/plugins/notify'
 
 export default [
   plugin,
   http,
   route,
+  notify,
 ]
