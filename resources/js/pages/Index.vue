4f925ccb7efa1ac1caadfef3eab5936950e4ca14
Grant Holle <hollegrant@gmail.com>
2021-01-08 23:20:36 +0800

    Add demo index page

---
+++ resources/js/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click.prevent="addSuccess">Success</button>
+    <button @click.prevent="addError">Error</button>
+    <notifications />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Notifications from '@/components/Notifications'
+
+export default defineComponent({
+  components: {
+    Notifications
+  },
+
+  methods: {
+    addSuccess () {
+      console.log(this.$success)
+      this.$success('This is a success!')
+    },
+
+    addError () {
+      this.$error('This is an error!')
+    }
+  }
+})
+</script>
