ed7ebf99d59d67fc293a82f444cfcaaed6d303e3
Grant Holle <hollegrant@gmail.com>
2021-01-09 15:12:53 +0800

    Allow the notification to be dismissed

--- resources/js/components/Notification.vue
+++ resources/js/components/Notification.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="ml-4 flex-shrink-0 flex">
           <!-- Close icon -->
-          <button @click.prevent="removeNotification(notification.id)" class="bg-white rounded-full inline-flex text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          <button @click.prevent="removeNotification(notification.id)" class="p-0.5 bg-white rounded-full inline-flex text-gray-400 hover:text-gray-500 hover:bg-gray-50 focus:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300">
             <span class="sr-only">Close</span>
             <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -62,6 +62,7 @@ export default defineComponent({
       color: notification.color || colors[notification.level] || colors.neutral,
       title: notification.title || titles[notification.level] || titles.neutral,
       icon: icons[notification.level] || icons.neutral,
+      state: store.state,
       removeNotification: store.removeNotification,
     }
   }
